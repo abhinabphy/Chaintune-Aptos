a5f61b32f876477240e404f9a67647bc850b00c8
abhinabphy <abhinabiitg@gmail.com>
2024-08-26 16:29:00 +0530

    hehe

--- apps/artists/src/components/TrackDetails/TrackDetails.tsx
+++ apps/artists/src/components/TrackDetails/TrackDetails.tsx
@@ -249,7 +249,7 @@ const TrackDetails: React.FC<TrackDetailsProps> = (props) => {
       handleUploadToIPFS(song, "SongAudio");
       handleUploadToIPFS(musicCoverFile, "SongImage");
       alert("Song Uploaded");
-      window.location.replace("http://localhost:3000/manageRelease");
+      // window.location.replace("http://localhost:3000/manageRelease");
     }
     setTrackName("");
     setMusicDescription("");
@@ -380,7 +380,7 @@ const TrackDetails: React.FC<TrackDetailsProps> = (props) => {
             headers: {
               // "Content-Type": `multipart/form-data; boundary=${formData._boundary}`,
               "Content-Type": `multipart/form-data;`,
-              Authorization: `Bearer ${process.env.NEXT_PUBLIC_JWT}`,
+              "Authorization": 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiJiMDIxYWI0Ny1lNzdiLTRmOTktOThjNC1mOTYxN2JjMmU0MzYiLCJlbWFpbCI6ImFiaGluYWJpaXRnQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaW5fcG9saWN5Ijp7InJlZ2lvbnMiOlt7ImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxLCJpZCI6IkZSQTEifSx7ImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxLCJpZCI6Ik5ZQzEifV0sInZlcnNpb24iOjF9LCJtZmFfZW5hYmxlZCI6ZmFsc2UsInN0YXR1cyI6IkFDVElWRSJ9LCJhdXRoZW50aWNhdGlvblR5cGUiOiJzY29wZWRLZXkiLCJzY29wZWRLZXlLZXkiOiIxOWUyMjYyZTk3MmMxNjk2MTAzMiIsInNjb3BlZEtleVNlY3JldCI6IjlhZmExMmVlYjM5MDIzY2VjMTllMmI0MWE0M2U3ZjI0ZDcxOWRhZDc5ZjI1NGI1OGUxYTM3ODVlNTc0NWE1MjUiLCJleHAiOjE3NTYwMjAyNDd9.kuFtbjmgGbE8dw-mZZ3FVJJoXEZF5irPN0CYF4HDDDg',
             },
           }
         );
